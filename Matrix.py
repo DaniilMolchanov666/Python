1cdcc27d2d4588c12502acc40001156b165e8e26
Cotton Pads <cottonpads1234@yandex.ru>
2023-04-05 18:40:04 +0300

    add matrix lab

---
+++ Matrix.py
@@ -0,0 +1,26 @@
+s = input('количество строк: ')
+c = input('количсевто столбцов:')
+
+# количество строк должно быть больше чем столбцов!
+
+string = int(s) # строки
+column = int(c) # столбцы
+
+matrix = [[0] * column] * string
+print('matrix =: ', matrix)
+
+index = 0
+
+for i in range(string):
+    if i % 2 != 0:
+        for j in range(column - 1, -1, -1):
+            index = index + 1
+            matrix[i][j] = index
+        print(matrix[j])
+    else:
+        for j in range(column):
+            index = index + 1
+            matrix[i][j] = index
+        print(matrix[j])
+
+
